2c505406131aa38739399a1326769066c8b80663
AshishKharel <ashishkharel58@gmail.com>
2021-04-04 11:43:20 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+const testVar={}
+function testFunc(){
+    return "hi";
+}
+function superbowlWin(array){
+    let foundObj= array.find(element =>{return element.result==="W"})
+    if(foundObj){
+        return foundObj.year
+    }
+    else{
+        return undefined;
+    }
+}
